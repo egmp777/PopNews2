755928adf169a18ea961409b23a2d45d6da8172b
egmp777 <egmp777@gmail.com>
2018-03-07 13:35:15 -0500

    First PopNews Commit

--- app/src/main/java/com/sevenrabbits/popnews/sync/AlgoUrlToTextTask.java
+++ app/src/main/java/com/sevenrabbits/popnews/sync/AlgoUrlToTextTask.java
@@ -4,6 +4,7 @@ package com.sevenrabbits.popnews.sync;
  * Created by elena on 31/08/17.
  */
 
+
 public class AlgoUrlToTextTask extends AlgorithmiaUrlToText {
     public AlgoUrlToTextTask(String apiKey, String algoUrl, String input, String[] articlePieces){
         super(apiKey,algoUrl, input, articlePieces);
